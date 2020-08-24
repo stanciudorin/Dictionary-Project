3ec7cef012b9793bc95012f3da9af2a0377c18aa
Dorin Stanciu <stanciudorin@yahoo.com>
2020-08-24 12:27:36 +0000

    Updating mongo_uri and mongo_dbname

--- app.py
+++ app.py
@@ -7,16 +7,12 @@ from bson.json_util import loads, dumps
 
 # Creating an instance of app and store it in the app variable
 app = Flask(__name__)
-SECRET_KEY = os.environ.get("SECRET_KEY", "")
-app.config['SECRET_KEY'] = SECRET_KEY
-app.config["MONGO_DBNAME"] = "DictionaryDB"
-app.config["MONGO_URI"] = os.environ.get("MONGO_URI", "mongodb://localhost")
 
-ALLOWED_HOSTS = ["dictionary-project.herokuapp.com"]
+app.config["MONGO_DBNAME"] = "DictionaryDB"
+app.config["MONGO_URI"] = "mongodb+srv://stanciudorin:conect86@myfirstcluster.awi3j.mongodb.net/DictionaryDB?retryWrites=true&w=majority"
 
 # Creating an instance of PyMongo
 mongo = PyMongo(app)
-
 
 # Displaying the list of categories and their words
 @app.route("/")
@@ -28,7 +24,6 @@ def show_words():
         category["words"] = words
     return render_template("words.html", categories=categories) # Returns everything in our categories collection
 
-
 @app.route("/edit")
 def edit():
     return render_template("edit.html", words=mongo.db.words.find()) # Returns everything in our words collection
@@ -39,7 +34,6 @@ def edit():
 def add_word():
     return render_template("addword.html", categories=mongo.db.categories.find()) #find function to fetch the categories
 
-
 # Function to add words into our dictionary
 @app.route("/insert_word", methods=["POST"])
 def insert_word():
@@ -47,14 +41,12 @@ def insert_word():
     words.insert_one(request.form.to_dict())
     return redirect(url_for("show_words"))
 
-
 # Displaying the interface of editing a word, display the page in an editable form
 @app.route("/edit_words/<word_id>")
 def edit_words(word_id):
     the_word = mongo.db.words.find_one({"_id": ObjectId(word_id)})
     all_categories = mongo.db.categories.find()
     return render_template("editword.html", word=the_word, categories=all_categories) 
-
 
 # Creating the function to update an existing word
 @app.route("/update_word/<word_id>", methods=["POST"])
@@ -69,26 +61,22 @@ def update_word(word_id):
 
     return redirect(url_for("show_words"))
 
-
 # Function to delete a word from our database and dictionary UX
 @app.route("/delete_word/<word_id>")
 def delete_word(word_id):
     mongo.db.words.remove({"_id": ObjectId(word_id)})
     return redirect(url_for("show_words"))
 
-
 # Displaying the Categories list
 @app.route("/get_categories")
 def get_categories():
     return render_template("categories.html", categories=mongo.db.categories.find())
-
 
 # Displaying the editable interface
 @app.route("/edit_category/<category_id>")
 def edit_category(category_id):
     return render_template("editcategory.html",
     category=mongo.db.categories.find_one({"_id": ObjectId(category_id)}))
-
 
 # Function to update the category
 @app.route("/update_category/<category_id>", methods=["POST"])
@@ -98,8 +86,9 @@ def update_category(category_id):
         {"category_name": request.form.get("category_name")})
     return redirect(url_for("get_categories"))
 
-
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
+
+
